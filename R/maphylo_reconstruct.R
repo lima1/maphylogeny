142e42686d271b5a093b9d47ecb4c03a750327d0
lima1 <mriester@gmx.de>
2011-03-12 11:55:27 -0500

    snow for reconstruct

--- R/maphylo_reconstruct.R
+++ R/maphylo_reconstruct.R
@@ -1,5 +1,13 @@
 maphylo_reconstruct <-
-function(dists,pm=nj, ...) {
-    lapply(dists, function(dist) pm(dist$dist, ...))
+function(dists,snow=FALSE,pm=nj, ...) {
+
+    pmw <- function(dist, ...) pm(dist$dist, ...)
+    
+    if ("cluster" %in% class(snow)) {
+        clusterEvalQ(snow, library(ape))
+        return(parLapply(snow, dists, pmw, ...))
+    }    
+    
+    lapply(dists, pmw, ...)
 }
 
