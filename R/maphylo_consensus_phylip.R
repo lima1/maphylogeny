c6b24d3f393bf64345bfb85bc2e703aebf2ca8cb
lima1 <mriester@gmx.de>
2011-03-21 22:33:53 -0400

    phylip

--- R/maphylo_consensus_phylip.R
+++ R/maphylo_consensus_phylip.R
@@ -1,8 +1,8 @@
 maphylo_consensus_phylip <-
 function(trees, outfile="phy", outgroup=1) {
     outfileall = paste(outfile,".trees",sep="")
-    sapply(1:length(trees), function(i) write.tree(trees[[i]],
-    file=outfileall, append=TRUE))
+    if (file.exists(outfileall)) file.remove(outfileall)
+    sapply(trees, write.tree, file=outfileall, append=TRUE)
     system(paste("fconsense -intreefile ", outfileall, " -outgrno ",
     outgroup, 
     " -outfile ", outfile, ".fconsensus -outtreefile ",
